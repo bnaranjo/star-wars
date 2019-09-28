ed56ad6d1448e60f69cb1960454490809fc0a1bc
bnaranjo <banaranjo@poligran.edu.co>
2019-09-28 07:43:35 -0500

    se crean los diferentes componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { PeopleComponent } from './components/people/people.component';
+import { FilmsComponent } from './components/films/films.component';
+import { StarshipsComponent } from './components/starships/starships.component';
+import { VehiclesComponent } from './components/vehicles/vehicles.component';
+import { SpeciesComponent } from './components/species/species.component';
+import { PlanetsComponent } from './components/planets/planets.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PeopleComponent,
+    FilmsComponent,
+    StarshipsComponent,
+    VehiclesComponent,
+    SpeciesComponent,
+    PlanetsComponent
   ],
   imports: [
     BrowserModule
